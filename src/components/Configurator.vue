64139b6057832dcd96b629e00d7f9c1ce35c42d0
yasiru98 <yasiru043@gmail.com>
2021-07-31 05:07:08 -0400

    fixed responsivity

--- src/components/Configurator.vue
+++ src/components/Configurator.vue
@@ -18,13 +18,13 @@
     <v-container>
       <v-row no-gutters>
         <v-col sm="0" md="1"></v-col>
-        <v-col sm="6" md="5">
+        <v-col sm="12" md="5">
           <v-card elevation="2">
             <div id="container"></div>
           </v-card>
         </v-col>
 
-        <v-col sm="6" md="5">
+        <v-col sm="12" md="5">
           <v-card class="ui-card" elevation="2">
             <v-row class="space-row" no-gutters>
               <v-col sm="12" md="12">
@@ -34,14 +34,14 @@
             <v-divider></v-divider>
             <v-row no-gutters align="center" justify="center" class="pt-10">
               <v-col sm="0" md="2"></v-col>
-              <v-col sm="6" md="5">
+              <v-col sm="12" md="5">
                 <button icon v-on:click="changeMaterial(`N01_BaseColor_Beech.png`)">
                   <v-avatar size="62">
                     <img src="/textures/beech.jpg">
                   </v-avatar>&nbsp;Original
                 </button>
               </v-col>
-              <v-col sm="6" md="5">
+              <v-col sm="12" md="5">
                 <button icon v-on:click="changeMaterial(`N01_BaseColor_Oak.png`)">
                   <v-avatar size="62">
                     <v-hover>
@@ -50,18 +50,19 @@
                   </v-avatar>&nbsp;Oak
                 </button>
               </v-col>
+              <v-col class="space-col" sm="0" md="2"></v-col>
             </v-row>
 
             <v-row id="row2" no-gutters align="center" justify="center">
               <v-col sm="0" md="2"></v-col>
-              <v-col sm="6" md="5">
+              <v-col sm="12" md="5">
                 <button v-on:click="changeMaterial(`tree5.jpg`)">
                   <v-avatar size="62">
                     <img src="/textures/tree5.jpg">
                   </v-avatar>&nbsp;Dark Wood
                 </button>
               </v-col>
-              <v-col sm="6" md="5">
+              <v-col sm="12" md="5">
                 <button icon v-on:click="changeMaterial(`N01_BaseColor_Black.png`)">
                   <v-avatar color="black" size="62"></v-avatar>&nbsp;Black
                 </button>
@@ -69,7 +70,7 @@
             </v-row>
           </v-card>
         </v-col>
-        <v-col sm="0" md="1"></v-col>
+        <v-col class="space-col" sm="0" md="2"></v-col>
       </v-row>
     </v-container>
   </div>
@@ -91,7 +92,7 @@ export default {
       controls: null,
       chair: null,
       originalChair: null,
-      container:null,
+      container: null
     };
   },
   methods: {
@@ -109,18 +110,17 @@ export default {
 
       this.scene.background = new THREE.Color("hsl(0, 100%, 100%)");
 
-      const ambientLight = new THREE.HemisphereLight(
-        0xffffff, 
-        0xffffff, 
-        1 
-      );
+      const ambientLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
       const mainLight = new THREE.DirectionalLight(0xffffff, 0.4);
       mainLight.position.set(0, 10, 10);
       this.scene.add(ambientLight, mainLight);
 
       this.renderer = new THREE.WebGLRenderer({ antialias: true });
       this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+      this.renderer.setSize(
+        this.container.clientWidth,
+        this.container.clientHeight
+      );
       this.container.appendChild(this.renderer.domElement);
 
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -134,7 +134,6 @@ export default {
       this.controls.enablePan = false;
       const loader = new GLTFLoader();
 
-
       //load chair model
       loader.load(
         // resource URL
@@ -144,7 +143,6 @@ export default {
           this.chair = gltf.scene;
           this.originalChair = gltf.scene;
           this.scene.add(this.chair);
-
         },
         // called while loading is progressing
         function(xhr) {
@@ -180,16 +178,20 @@ export default {
     },
     //Handle window resize, set camera aspect/renderer size
     resizeHandler: function() {
-    this.camera.aspect = this.container.clientWidth, this.container.clientHeight;
-    this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
-  }
+      (this.camera.aspect = this.container.clientWidth),
+        this.container.clientHeight;
+      this.renderer.setSize(
+        this.container.clientWidth,
+        this.container.clientHeight
+      );
+    }
   },
   mounted() {
     this.init();
     this.animate();
   },
   created() {
-    //detect window resize 
+    //detect window resize
     window.addEventListener("resize", this.resizeHandler);
   },
   destroyed() {
@@ -214,6 +216,11 @@ export default {
   padding-top: 10%;
 }
 button:hover {
-   font-weight: bold;
+  font-weight: bold;
+}
+@media (max-width: 600px) {
+  .space-col {
+    display: none;
+  }
 }
 </style>
