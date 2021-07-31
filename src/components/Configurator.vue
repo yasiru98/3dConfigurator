b83f275647cf0c7abfe2c7b7756bb4c393d18f34
yasiru98 <yasiru043@gmail.com>
2021-07-31 06:33:04 -0400

    ui update

--- src/components/Configurator.vue
+++ src/components/Configurator.vue
@@ -64,7 +64,7 @@
               </v-col>
               <v-col sm="12" md="5">
                 <button icon v-on:click="changeMaterial(`N01_BaseColor_Black.png`)">
-                  <v-avatar color="black" size="62"></v-avatar>&nbsp;Black ME
+                  <v-avatar color="black" size="62"></v-avatar>&nbsp;Black Metal
                 </button>
               </v-col>
             </v-row>
